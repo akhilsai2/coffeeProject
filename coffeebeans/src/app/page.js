7bb1f04efb660fd5e8f68854c0d27ee528130b6a
Sai Akhil <saiakil456@gmail.com>
2025-04-10 22:32:31 +0530

    project setup coffee beans

---
+++ coffeebeans/src/app/page.js
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <h1>Welcome to Our Home Page</h1>
+  );
+}
---
+++ coffeebeans/src/app/product/page.js
@@ -0,0 +1,3 @@
+export default function ProductPage() {
+    return <h1>Our Products</h1>;
+  }
